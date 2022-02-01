1c77c82d689e7aeddc1fd8d295f240f655520385
Anthony Sofio <asofio@titusoil.com>
2022-02-01 14:05:05 -0600

    updated .sh

--- openhacksolution/localrun.sh
+++ openhacksolution/localrun.sh
@@ -15,7 +15,7 @@ docker network create $DOCKER_NETWORK
 docker run --network $DOCKER_NETWORK -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$SQL_PASSWORD" -p 1433:1433 \
 --name sqlserver -d mcr.microsoft.com/mssql/server:2017-latest 
 
-# 4) Create myDrivingDB database
+# 3) Create myDrivingDB database
 
 docker exec \
  -it $SQL_SERVER \
@@ -23,17 +23,9 @@ docker exec \
  -S localhost -U SA -P $SQL_PASSWORD \
  -Q "CREATE DATABASE mydrivingDB"
 
-# CREATE DATABASE myDrivingDB;
-
-# 7) Run dataload against local SQL Server
+# 4) Run dataload against local SQL Server
 
 docker run --network $DOCKER_NETWORK -e SQLFQDN=$SQL_SERVER -e SQLUSER=SA -e SQLPASS=lM3am0Rf9 -e SQLDB=myDrivingDB registryacm8754.azurecr.io/dataload:1.0
-
-# 3) Adjust sa name and change to sqladmin 
-
-# ALTER LOGIN [sa] WITH NAME = [sqladmin]
-
-
 
 # 5) Build POI API container
 
@@ -43,10 +35,8 @@ docker build -f Dockerfile -t "tripinsights/poi:1.0" .
 
 docker run -d -p 8080:80 --name poi --network $DOCKER_NETWORK \
 -e "SQL_PASSWORD=$SQL_PASSWORD" -e "SQL_SERVER=$SQL_SERVER" -e "SQL_DBNAME=myDrivingDB" \
--e "SQL_USER=SA" tripinsights/poi:1.0
+-e "SQL_USER=SA" -e "ASPNETCORE_ENVIRONMENT=Local" tripinsights/poi:1.0
 
-
-
-# 8) Verify that data can be pull from POI API (using SQL Server backend)
+# 7) Verify that data can be pull from POI API (using SQL Server backend)
 
 curl -i -X GET 'http://localhost:8080/api/poi' 
