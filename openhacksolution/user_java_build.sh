70e523a3a11f3a8ebd279d6a9a6214f3e23e8664
Anthony Sofio <asofio@titusoil.com>
2022-02-02 10:21:48 -0600

    increased sleep

--- openhacksolution/user_java_build.sh
+++ openhacksolution/user_java_build.sh
@@ -19,7 +19,7 @@ docker build --no-cache --build-arg IMAGE_VERSION="1.0" --build-arg IMAGE_CREATE
 
 docker run -d -p $api_port:80 --name $image_alias --network $DOCKER_NETWORK -e "SQL_PASSWORD=$SQL_PASSWORD" -e "SQL_SERVER=$SQL_SERVER" -e "SQL_DBNAME=myDrivingDB" -e "SQL_USER=SA" $image_name
 
-sleep 2
+sleep 5
 
 curl -i -X GET "http://localhost:$api_port/api/docs/user-java"
 
